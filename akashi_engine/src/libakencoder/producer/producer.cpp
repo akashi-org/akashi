5e3e560f3e56c657c5f75d027a284935f2e829f0
crux14 <michaels310t@gmail.com>
2021-02-09 01:15:23 +0000

    akencoder: impl decode process for producer thread

--- akashi_engine/src/libakencoder/producer/producer.cpp
+++ akashi_engine/src/libakencoder/producer/producer.cpp
@@ -8,6 +8,10 @@
 #include <libakcore/path.h>
 #include <libakstate/akstate.h>
 #include <libakeval/akeval.h>
+#include <libakcodec/akcodec.h>
+#include <libakbuffer/avbuffer.h>
+#include <libakbuffer/video_queue.h>
+#include <libakbuffer/audio_queue.h>
 
 using namespace akashi::core;
 
@@ -23,6 +27,10 @@ namespace akashi {
             Rational fps;
             Rational duration;
             core::Path entry_path{""};
+            core::owned_ptr<codec::AKDecoder> decoder;
+            core::owned_ptr<buffer::AVBuffer> buffer;
+
+            bool decode_ended = false;
         };
 
         static void exit_thread(ExitContext& exit_ctx) {
@@ -34,24 +42,25 @@ namespace akashi {
 
         static EncodeContext create_encode_context(ProduceLoopContext& ctx,
                                                    core::borrowed_ptr<eval::AKEval> eval) {
+            Rational start_pts = Rational(0, 1);
             Rational fps;
             core::Path entry_path{""};
+            RenderProfile profile;
             {
                 std::lock_guard<std::mutex> lock(ctx.state->m_prop_mtx);
                 entry_path = ctx.state->m_prop.eval_state.config.entry_path;
                 fps = ctx.state->m_prop.fps;
-            }
+                profile = eval->render_prof(entry_path.to_abspath().to_str());
 
-            auto profile = eval->render_prof(entry_path.to_abspath().to_str());
-            {
-                std::lock_guard<std::mutex> lock(ctx.state->m_prop_mtx);
                 ctx.state->m_prop.render_prof = profile;
             }
 
-            return {.cur_pts = Rational(0, 1),
+            return {.cur_pts = start_pts,
                     .fps = fps,
                     .duration = to_rational(profile.duration),
-                    .entry_path = entry_path};
+                    .entry_path = entry_path,
+                    .decoder = make_owned<codec::AKDecoder>(profile.atom_profiles, start_pts),
+                    .buffer = make_owned<buffer::AVBuffer>(borrowed_ptr(ctx.state))};
         }
 
         static void update_encode_context(EncodeContext& encode_ctx) {
@@ -61,6 +70,79 @@ namespace akashi {
         static bool can_produce(const EncodeContext& encode_ctx) {
             // [TODO] exclusive?
             return encode_ctx.cur_pts <= encode_ctx.duration;
+        }
+
+        static bool exec_decode(ProduceLoopContext& ctx, EncodeContext& encode_ctx) {
+            if (encode_ctx.decode_ended) {
+                return true;
+            }
+
+            codec::DecodeArg decode_args;
+            {
+                std::lock_guard<std::mutex> lock(ctx.state->m_prop_mtx);
+                decode_args.out_audio_spec = ctx.state->m_atomic_state.audio_spec.load();
+                decode_args.decode_method = ctx.state->m_atomic_state.decode_method.load();
+                // [TODO] this value should be changed
+                decode_args.video_max_queue_count = ctx.state->m_prop.video_max_queue_count;
+            }
+
+            while (true) {
+                if (!ctx.state->get_video_decode_ready() || !ctx.state->get_audio_decode_ready()) {
+                    break;
+                }
+                auto decode_res = encode_ctx.decoder->decode(decode_args);
+
+                switch (decode_res.result) {
+                    case codec::DecodeResultCode::ERROR: {
+                        AKLOG_ERROR("decode error, code: {}", decode_res.result);
+                        return false;
+                    }
+                    case codec::DecodeResultCode::DECODE_ENDED: {
+                        AKLOG_INFO("DecodeLoop::decode_thread(): ended, code: {}",
+                                   decode_res.result);
+                        encode_ctx.decode_ended = true;
+                        return true;
+                    }
+                    case codec::DecodeResultCode::DECODE_LAYER_EOF:
+                    case codec::DecodeResultCode::DECODE_LAYER_ENDED:
+                    case codec::DecodeResultCode::DECODE_STREAM_ENDED:
+                    case codec::DecodeResultCode::DECODE_ATOM_ENDED:
+                    case codec::DecodeResultCode::DECODE_AGAIN:
+                    case codec::DecodeResultCode::DECODE_SKIPPED: {
+                        AKLOG_INFO("decode skipped or layer ended, code: {}, uuid: {}",
+                                   decode_res.result, decode_res.layer_uuid);
+                        break;
+                    }
+                    case codec::DecodeResultCode::OK: {
+                        switch (decode_res.buffer->prop().media_type) {
+                            case buffer::AVBufferType::VIDEO: {
+                                const auto comp_layer_uuid =
+                                    decode_res.layer_uuid + std::to_string(0);
+                                encode_ctx.buffer->vq->enqueue(comp_layer_uuid,
+                                                               std::move(decode_res.buffer));
+                                break;
+                            }
+                            case buffer::AVBufferType::AUDIO: {
+                                const auto comp_layer_uuid =
+                                    decode_res.layer_uuid + std::to_string(0);
+                                encode_ctx.buffer->aq->enqueue(comp_layer_uuid,
+                                                               std::move(decode_res.buffer));
+                                break;
+                            }
+                            default: {
+                            }
+                        }
+
+                        break;
+                    }
+                    default: {
+                        AKLOG_ERROR("invalid result code found, code: {}",
+                                    static_cast<int>(decode_res.result));
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
 
         static std::vector<core::FrameContext>
@@ -98,9 +180,13 @@ namespace akashi {
                 auto frame_ctx = pull_frame_context(borrowed_ptr(eval), encode_ctx);
 
                 // decode
-                // render
-                // enqueue
+                if (!exec_decode(ctx, encode_ctx)) {
+                    break;
+                }
 
+                // render
+
+                // enqueue
                 ctx.queue->enqueue({encode_ctx.cur_pts.to_decimal()});
             }
 
