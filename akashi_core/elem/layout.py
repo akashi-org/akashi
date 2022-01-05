2fa293fa6f6ce1fb2932959b8c614236f0de9cb2
crux14 <michaels310t@gmail.com>
2022-01-05 22:59:10 +0000

    corelib: improve vstack, hstack

--- akashi_core/elem/layout.py
+++ akashi_core/elem/layout.py
@@ -12,17 +12,26 @@ LayoutInfo: tp.TypeAlias = PositionField
 LayoutFn: tp.TypeAlias = tp.Callable[[int, int], LayoutInfo]
 
 
-def vstack(item_width: int = -1) -> LayoutFn:
+def vstack(item_width: int = -1, layout_height: int | None = None, item_offsets: tuple[int, int] = (0, 0)) -> LayoutFn:
+    if not layout_height:
+        layout_height = ak_height()
+
     def layout(lane_idx: int, lane_size: int) -> LayoutInfo:
-        pos = (ak_width() // 2, int((ak_height() // lane_size) * (lane_idx + 0.5)))
-        layer_size = (item_width, (ak_height() // lane_size))
-        return LayoutInfo(pos, -1, layer_size)
+        px = (ak_width() // 2) + item_offsets[0]
+        py = int((layout_height // lane_size) * (lane_idx + 0.5)) + \
+            ((ak_height() - layout_height) // 2) + item_offsets[1]
+        layer_size = (item_width, (layout_height // lane_size))
+        return LayoutInfo((px, py), -1, layer_size)
     return layout
 
 
-def hstack(item_height: int = -1) -> LayoutFn:
+def hstack(item_height: int = -1, layout_width: int | None = None, item_offsets: tuple[int, int] = (0, 0)) -> LayoutFn:
+    if not layout_width:
+        layout_width = ak_width()
+
     def layout(lane_idx: int, lane_size: int) -> LayoutInfo:
-        pos = (int((ak_width() // lane_size) * (lane_idx + 0.5)), ak_height() // 2)
-        layer_size = ((ak_width() // lane_size), item_height)
-        return LayoutInfo(pos, -1, layer_size)
+        px = int((layout_width // lane_size) * (lane_idx + 0.5)) + ((ak_width() - layout_width) // 2) + item_offsets[0]
+        py = (ak_height() // 2) + item_offsets[1]
+        layer_size = ((layout_width // lane_size), item_height)
+        return LayoutInfo((px, py), -1, layer_size)
     return layout
