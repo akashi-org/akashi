689809468e9693eb192f1fcbbdafd36504f8281b
crux14 <michaels310t@gmail.com>
2022-06-11 01:49:42 +0000

    corelib & cli: v0.4.2

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def readme():
 
 setup(
     name='akashi-engine',
-    version='0.4.1',
+    version='0.4.2',
     description='A next-generation video editor',
     long_description=readme(),
     long_description_content_type='text/markdown',
