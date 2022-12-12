233979009dd25cd0728913058550be4b942cd2b2
crux14 <michaels310t@gmail.com>
2022-12-12 02:41:29 +0000

    corelib: Use reproducible random uuids

--- akashi_core/elem/uuid.py
+++ akashi_core/elem/uuid.py
@@ -1,8 +1,19 @@
 from uuid import uuid4
-from typing import NewType
+from uuid import UUID as _ORIG_UUID
+import typing as tp
+import random
 
-UUID = NewType('UUID', str)
+
+UUID = tp.NewType('UUID', str)
+
+
+def _default_gen() -> _ORIG_UUID:
+    return uuid4()
+
+
+def _custom_gen() -> _ORIG_UUID:
+    return _ORIG_UUID(int=random.getrandbits(128), version=4)
 
 
 def gen_uuid() -> UUID:
-    return UUID(str(uuid4()))
+    return UUID(str(_custom_gen()))
