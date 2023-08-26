f0682eaf79ca2533ac618582d618e9dbaa138884
crux14 <michaels310t@gmail.com>
2023-08-26 14:06:50 +0000

    cli: Deprecate `load_module()` in favor of `exec_module()`

--- akashi_cli/config_parser.py
+++ akashi_cli/config_parser.py
@@ -1,6 +1,7 @@
-from importlib.machinery import SourceFileLoader
+import importlib.util
 from typing import TYPE_CHECKING, Any
 from os import path
+import sys
 
 if TYPE_CHECKING:
     from akashi_core.config import AKConf
@@ -8,7 +9,13 @@ if TYPE_CHECKING:
 
 def config_parse(conf_path: str) -> 'AKConf':
 
-    akconf: Any = SourceFileLoader("akconfig", path.abspath(conf_path)).load_module()  # type: ignore
+    mod_name = 'akconfig'
+    spec = importlib.util.spec_from_file_location(mod_name, path.abspath(conf_path))
+    if not spec or not spec.loader:
+        raise Exception('akashi: error: spec not found')
+    akconf = importlib.util.module_from_spec(spec)
+    sys.modules[mod_name] = akconf
+    spec.loader.exec_module(akconf)
     if not hasattr(akconf, '__akashi_export_config_fn'):
         raise Exception('akashi: error: No config function found. Perhaps you forget to add the export decorator?')
     return getattr(akconf, '__akashi_export_config_fn')()
