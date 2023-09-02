akashi-org/akashi

commit 9295c746bd436f9ee53c7a25a5e6d39b1ab56cd3

wip: Fix bugs with media layer's start, end field
* But the impl in libakcodec apparently needs to be fixed later

akashi_engine/src/libakcodec/backend/ffmpeg/pts.cpp
@@ -37,9 +37,13 @@ namespace akashi {
 
             m_frame_rpts = pts_to_rational(adjusted_pts, frame_time_base);
 
+            auto media_offset = m_input_src->loop_cnt == 0
+                                    ? m_input_src->layer_prof.layer_local_offset
+                                    : core::Rational(0l);
+
             m_frame_pts = (m_input_src->act_dur * m_input_src->loop_cnt) +
                           rpts_to_pts(m_frame_rpts, m_input_src->layer_prof.from,
-                                      m_input_src->layer_prof.start);
+                                      m_input_src->layer_prof.start + media_offset);
         }
 
         bool PTSSet::is_valid(void) const {

akashi_core/elem/layer/unit.py
@@ -313,14 +313,14 @@ class SpatialFrameGuard:
                 cur_layer.layer_local_offset = new_layer_local_offset
                 cur_layer._duration = new_duration
 
-                # if cur_layer.layer_local_offset > sec(0) and isinstance(cur_layer, HasMediaField):
+                if cur_layer.layer_local_offset > sec(0) and isinstance(cur_layer, HasMediaField):
 
-                #     media_slice_dur = cur_layer.media.end - cur_layer.media.start
-                #     media_slice_part = int(cur_layer.layer_local_offset / media_slice_dur)
-                #     cur_layer.media.start = cur_layer.layer_local_offset - (media_slice_part * media_slice_dur)
-                #     cur_layer.media.end += cur_layer.layer_local_offset
-                #     cur_layer.media._span_cnt = None
-                #     cur_layer.media._span_dur = cur_layer._duration
+                    media_slice_dur = cur_layer.media.end - cur_layer.media.start
+                    media_slice_part = int(cur_layer.layer_local_offset / media_slice_dur)
+                    cur_layer.layer_local_offset = cur_layer.layer_local_offset - (media_slice_part * media_slice_dur)
+                    # cur_layer.media.end += cur_layer.layer_local_offset
+                    # cur_layer.media._span_cnt = None
+                    # cur_layer.media._span_dur = cur_layer._duration
 
                 living_layer_indices.append(layer_idx)
             else:

akashi_engine/src/libakbuffer/video_queue.cpp
@@ -78,7 +78,7 @@ namespace akashi {
                     auto diff = m_qmap[layer_uuid].buf.front()->prop().pts - pts;
 
                     Rational drop_threshold = Rational(0, 1000);   // 0ms
-                    Rational skip_threshold = Rational(100, 1000); // 10ms
+                    Rational skip_threshold = Rational(100, 1000); // 100ms
 
                     // when the necessary pts is greater than that of the queue
                     if (diff < drop_threshold) {
@@ -158,9 +158,19 @@ namespace akashi {
                     while (contains && !m_qmap[layer_uuid].buf.empty()) {
                         auto diff = m_qmap[layer_uuid].buf.front()->prop().pts - seek_pts;
 
-                        Rational seek_threshold = Rational(0, 1);
+                        Rational drop_threshold = Rational(0, 1000);   // 0ms
+                        Rational skip_threshold = Rational(100, 1000); // 100ms
 
-                        if (diff >= seek_threshold) {
+                        if (diff < drop_threshold) {
+                            // logging?
+                        } else if (diff > skip_threshold) {
+                            AKLOG_INFO("Seeked Failed want: {} , front: {}, uuid: {}",
+                                       seek_pts.to_decimal(),
+                                       m_qmap[layer_uuid].buf.front()->prop().pts.to_decimal(),
+                                       layer_uuid);
+                            res = false;
+                            break;
+                        } else {
                             AKLOG_INFO("Seeked want: {} , front: {}, uuid: {}",
                                        seek_pts.to_decimal(),
                                        m_qmap[layer_uuid].buf.front()->prop().pts.to_decimal(),

akashi_engine/src/libakcodec/backend/ffmpeg/source.cpp
@@ -39,7 +39,11 @@ namespace akashi {
 
             auto act_from = m_input_src.layer_prof.from +
                             (core::Rational(m_input_src.loop_cnt, 1) * m_input_src.act_dur);
-            auto seek_rpts = m_input_src.layer_prof.start + (decode_start - act_from);
+
+            auto media_offset = m_input_src.layer_prof.layer_local_offset;
+
+            auto seek_rpts =
+                (media_offset + m_input_src.layer_prof.start) + (decode_start - act_from);
 
             if (seek_rpts > Rational(0, 1)) {
                 if (!this->seek(seek_rpts)) {
@@ -215,6 +219,10 @@ namespace akashi {
                 m_input_src.loop_cnt = 0;
             } else {
                 m_input_src.loop_cnt = (r_dts / m_input_src.act_dur).to_decimal();
+                if (m_input_src.layer_prof.layer_local_offset > 0 and
+                    r_dts > (m_input_src.act_dur - m_input_src.layer_prof.layer_local_offset)) {
+                    m_input_src.loop_cnt += 1;
+                }
             }
 
             // input_src[i].io_ctx = new IOContext(input_paths[i]);

akashi_engine/src/libakcore/element.h
@@ -151,6 +151,7 @@ namespace akashi {
         struct LayerProfile {
             LayerType type;
             Rational from = core::Rational(0, 1);
+            Rational layer_local_offset = core::Rational(0, 1);
             Rational to = core::Rational(0, 1);
             std::string uuid;
             std::string src;

akashi_engine/src/libakeval/backend/python/elem/elem_proxy.cpp
@@ -34,6 +34,7 @@ namespace akashi {
             core::LayerProfile computed{};
             computed.uuid = m_layer_ctx.uuid;
             computed.from = m_layer_ctx.from + base_time;
+            computed.layer_local_offset = m_layer_ctx.layer_local_offset;
             computed.to = m_layer_ctx.to + base_time;
             computed.type = static_cast<core::LayerType>(m_layer_ctx.type);
             switch (computed.type) {

commit 03328246984e1f514b5e97b22829980152715538

akcodec: Fix a regression introduced by 9295c74

akashi_engine/src/libakcodec/backend/ffmpeg/pts.cpp
@@ -21,7 +21,7 @@ namespace akashi {
         akashi::core::Rational rpts_to_pts(const akashi::core::Rational& rpts,
                                            const akashi::core::Rational& from,
                                            const akashi::core::Rational& start) {
-            return rpts - (start - from);
+            return from + (rpts - start);
         }
 
         PTSSet::PTSSet(const InputSource* input_src, const AVFrame* frame, const int stream_index) {
@@ -37,13 +37,10 @@ namespace akashi {
 
             m_frame_rpts = pts_to_rational(adjusted_pts, frame_time_base);
 
-            auto media_offset = m_input_src->loop_cnt == 0
-                                    ? m_input_src->layer_prof.layer_local_offset
-                                    : core::Rational(0l);
-
             m_frame_pts = (m_input_src->act_dur * m_input_src->loop_cnt) +
                           rpts_to_pts(m_frame_rpts, m_input_src->layer_prof.from,
-                                      m_input_src->layer_prof.start + media_offset);
+                                      m_input_src->layer_prof.start) -
+                          m_input_src->layer_prof.layer_local_offset;
         }
 
         bool PTSSet::is_valid(void) const {